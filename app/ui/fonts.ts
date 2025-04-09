537dd00be4c022764e7df51243d4d2ca83a23fe2
Felipe Assis <felipe.assis.2002@gmail.com>
2025-04-09 11:02:50 -0300

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Cinzel, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const cinzel = Cinzel({subsets: ['latin']});
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] }); 
